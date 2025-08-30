5962c82338f50854501b03c7fb1f99e47e91f54c
Ezequiel Moraga <ezequielmoraga3572@gmail.com>
2025-08-29 23:41:06 -0300

    agregar imagenes

---
+++ script.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const secs = document.querySelectorAll('.fade-in-section');
+  const obs = new IntersectionObserver(entries => {
+    entries.forEach(e => {
+      if (e.isIntersecting) {
+        e.target.classList.add('visible');
+        obs.unobserve(e.target);
+      }
+    });
+  }, { threshold: 0.2 });
+
+  secs.forEach(s => obs.observe(s));
+});
+
+// Función que se llama al hacer clic en el botón "Más"
+function toggleMenu() {
+  // Agrega o quita la clase "show" al menú desplegable
+  document.getElementById("miDropdown").classList.toggle("show");
+}
+
+// Si el usuario hace clic fuera del botón, se cierra el menú
+window.onclick = function(e) {
+  // Si el clic NO fue sobre el botón
+  if (!e.target.matches('.dropbtn')) {
+    // Oculta todos los dropdowns
+    const dropdowns = document.getElementsByClassName("dropdown-content");
+    for (let i = 0; i < dropdowns.length; i++) {
+      dropdowns[i].classList.remove('show');
+    }
+  }
+};
+
