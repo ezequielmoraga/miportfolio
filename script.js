2a6ecfade1b51eef5e90bb9ce9cf1b83a1a180c8
Ezequiel Moraga <ezequielmoraga3572@gmail.com>
2025-09-20 23:58:11 -0300

    tarjetas

--- script.js
+++ script.js
@@ -44,3 +44,48 @@ window.embeddedChatbotConfig = {
   s.defer = true;
   document.body.appendChild(s);
 })();
+
+
+
+// certificaciones carrusel
+
+let indexActual = 0;
+
+function actualizarCarrusel() {
+  const cards = document.querySelectorAll('.curso-card');
+  const total = cards.length;
+
+  cards.forEach((card, i) => {
+    card.classList.remove("active", "left", "right", "hidden");
+
+    if (i === indexActual) {
+      card.classList.add("active"); // centro
+    } else if (i === (indexActual - 1 + total) % total) {
+      card.classList.add("left"); // izquierda
+    } else if (i === (indexActual + 1) % total) {
+      card.classList.add("right"); // derecha
+    } else {
+      card.classList.add("hidden"); // fuera de vista
+    }
+  });
+}
+
+function moverCarrusel(direccion) {
+  const cards = document.querySelectorAll('.curso-card');
+  indexActual = (indexActual + direccion + cards.length) % cards.length;
+  actualizarCarrusel();
+}
+
+document.addEventListener("DOMContentLoaded", actualizarCarrusel);
+
+
+function abrirCertificado(src) {
+  const popup = document.getElementById("popup-certificado");
+  const popupImg = document.getElementById("popup-img");
+  popupImg.src = src;
+  popup.style.display = "flex";
+}
+
+function cerrarCertificado() {
+  document.getElementById("popup-certificado").style.display = "none";
+}
