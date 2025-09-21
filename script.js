b02b5df6d3575c080ea93e12d811a7c2cf358810
Ezequiel Moraga <ezequielmoraga3572@gmail.com>
2025-09-21 00:36:50 -0300

    nav

--- script.js
+++ script.js
@@ -114,3 +114,12 @@ if (carrusel) {
     }
   });
 }
+
+
+// Menú hamburguesa
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.getElementById("nav-links");
+
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("show");
+});
